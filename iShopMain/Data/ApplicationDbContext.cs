4afae7e0e9b0829b35f1f02bf4768d0f99a68990
Евгений Власов <zhenya.vlasov2002@mail.ru>
2023-04-08 23:29:16 +0300

    Добавьте файлы проекта.

---
+++ iShopMain/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using iShopMain.Models.Entity.UserInfo;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace iShopMain.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<AppUser> users { get; set; }
+        public DbSet<Account> accounts { get; set; }
+        public DbSet<Information> informations { get; set; }
+        public DbSet<Role> roles { get; set; }
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+            ApplicationDbInitializer.Initialize(this);
+        }
+
+    }
+}
